cc65952fbef5d28d5681920f48d591f463526a42
AbrahamRH <abrahamrzhz@gmail.com>
2023-03-12 10:10:36 -0600

    Now the API.js file supports all the Methods needed

--- front-end-project/src/Utils/API.js
+++ front-end-project/src/Utils/API.js
@@ -1,25 +1,29 @@
 const url = "http://localhost:9090";
 
-export default async function handleAPI(method, endpoint = "", body = {}) {
-  if (method === "POST") {
-    try {
-      const response = await fetch(url + endpoint, settings(body));
-      const data = await response.json();
-      return data;
-    } catch (e) {
-      return e;
-    }
+export default async function handleAPI(
+  method,
+  endpoint = "/todos",
+  body
+) {
+  try {
+    const response = await fetch(url + endpoint, settings(method, body));
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    return e;
   }
 }
 
-function settings(data = {}) {
-  const postSettings = {
-    method: "POST",
+function settings(method, body) {
+  if (method === "DELETE" || method === "GET") return { method: method };
+
+  const Settings = {
+    method: method,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(body),
   };
-  return postSettings
+  return Settings;
 }
