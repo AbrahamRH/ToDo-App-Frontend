65cc2317d5e06b1acc70562337447874af645424
AbrahamRH <abrahamrzhz@gmail.com>
2023-03-13 13:47:45 -0600

    adding a message if there is no content in the table

--- front-end-project/src/Components/TodosTable.jsx
+++ front-end-project/src/Components/TodosTable.jsx
@@ -6,30 +6,29 @@ import { TodoContext } from "../Context/TodosContext";
 export default function TodosTable() {
   const { todos } = useContext(TodoContext);
 
-  if (todos.length > 0) {
-    return (
-      <table className="todo-table">
-        <thead className="table-head">
-          <tr>
-            <th className="check"></th>
-            <th className="table-name">Name</th>
-            <th className="table-priority">Priority</th>
-            <th className="table-due-date">Due Date</th>
-            <th className="table-actions">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map((todo) => {
+  return (
+    <table className="todo-table">
+      <thead className="table-head">
+        <tr>
+          <th className="check"></th>
+          <th className="table-name">Name</th>
+          <th className="table-priority">Priority</th>
+          <th className="table-due-date">Due Date</th>
+          <th className="table-actions">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {(todos.length > 0 &&
+          todos.map((todo) => {
             return <TodoRow key={todo.id} todo={todo} />;
-          })}
-        </tbody>
-      </table>
-    );
-   } else {
-      return (
-        <h2>No content</h2>
-
-      )
-
-   }
+          })) || (
+          <tr>
+            <td colSpan={5} className="no-content">
+              No tasks yet
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 }
