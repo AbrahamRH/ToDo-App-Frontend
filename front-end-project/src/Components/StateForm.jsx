e8e6ad74745e815d39bd1dc605bbcef2c63ff61c
AbrahamRH <abrahamrzhz@gmail.com>
2023-03-14 22:50:54 -0600

    Changing the value of the "All" option in the State form

--- front-end-project/src/Components/StateForm.jsx
+++ front-end-project/src/Components/StateForm.jsx
@@ -4,7 +4,7 @@ export default function StateForm({ sendData }) {
       <label htmlFor="state" className="controls_label_state">State:</label>
       <select className="controls_state" name="state" onChange={sendData}>
         <option value="">All, Done, Undone</option>
-        <option value="All">All</option>
+        <option value="ALL">All</option>
         <option value="Done">Done</option>
         <option value="Undone">Undone</option>
       </select>
