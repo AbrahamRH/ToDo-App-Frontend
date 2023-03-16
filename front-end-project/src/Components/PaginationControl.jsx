f488be144393e89d6b8ce941ff27da64c73e3378
AbrahamRH <abrahamrzhz@gmail.com>
2023-03-16 14:25:23 -0600

    we can produce all the pages automatically

--- front-end-project/src/Components/PaginationControl.jsx
+++ front-end-project/src/Components/PaginationControl.jsx
@@ -5,15 +5,26 @@ import { TodoContext } from "../Context/TodosContext";
 export default function PaginationControl() {
   const { pagesInfo, selectPage, nextPage, prevPage, firstPage, lastPage } =
     useContext(TodoContext);
+
+  const buttons = []
+
+  for (let i = 0; i < pagesInfo.totalPages ; i++){
+    buttons.push(i)
+  }
+
   return (
     <div className="pagination-controls">
-      <button onClick={firstPage}>{"<<"}</button>
-      <button onClick={prevPage}>{"<"}</button>
-      <button onClick={firstPage}>1</button>
-      <button onClick={selectPage}>{pagesInfo.number + 1}</button>
-      <button onClick={lastPage}>{pagesInfo.totalPages}</button>
-      <button onClick={nextPage}>{">"}</button>
-      <button onClick={lastPage}>{">>"}</button>
+      <button onClick={firstPage}  className="go-to-first" >{"<<"}</button>
+      <button onClick={prevPage}   className="got-to-prev">{"<"}</button>
+      {buttons.map((number) => {
+        if(number === pagesInfo.number){
+          return <button onClick={ () => { selectPage(number) }} key={number} className="current-page">{number + 1}</button>
+        } else {
+          return <button onClick={ () => { selectPage(number) }} key={number} className="item-page">{number + 1}</button>
+        }
+      })}
+       <button onClick={nextPage} className="go-to-next hidden">{">"}</button>
+       <button onClick={lastPage} className="go-to-last">{">>"}</button>
     </div>
   );
 }
